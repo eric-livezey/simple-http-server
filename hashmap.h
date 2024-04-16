61deb836b633d744917eb5d266d8d26ef7b80fb1
eric-livezey <146676972+eric-livezey@users.noreply.github.com>
2024-04-16 19:51:42 -0400

    Update hashmap.h

--- hashmap.h
+++ hashmap.h
@@ -1,6 +1,7 @@
-#include <string.h>
 #include <stdlib.h>
-#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include "utils.h"
 
 #define DEFAULT_INITIAL_CAPACITY (1 << 4)
 #define MAXIMUM_CAPACITY (1 << 30)
@@ -10,7 +11,7 @@ typedef struct entry_s
 {
     char *key;
     char *value;
-} entry_t;
+} entry;
 
 typedef struct node_s
 {
@@ -18,11 +19,11 @@ typedef struct node_s
     char *key;
     char *value;
     struct node_s *next;
-} node_t;
+} hashmap_node;
 
 typedef struct hashmap_s
 {
-    entry_t **entryset;
+    entry **entryset;
     char **keyset;
     char **values;
     struct node_s **table;
@@ -31,52 +32,53 @@ typedef struct hashmap_s
     int modcount;
     int threshold;
     float loadfactor;
-} hashmap_t;
+    char ignorecase;
+} hashmap;
 
-int hash(char *s);
+int hash(char *s, char lower);
 
 int table_size_for(int cap);
 
-int node_init(int hash, char *key, char *value, node_t *next, node_t *node);
+int node_init(int hash, char *key, char *value, hashmap_node *next, hashmap_node *node);
 
-int hashmap_init(hashmap_t *hashmap);
+int hashmap_init(hashmap *hashmap, char ignorecase);
 
-void hashmap_destroy_cache(hashmap_t *map);
+void hashmap_free_cache(hashmap *map);
 
-void hashmap_clear(hashmap_t *map);
+void hashmap_clear(hashmap *map);
 
-int hashmap_destroy(hashmap_t *map);
+int hashmap_free_all(hashmap *map);
 
-node_t **hashmap_resize(hashmap_t *map);
+hashmap_node **hashmap_resize(hashmap *map);
 
-entry_t **hashmap_entry_set(hashmap_t *map);
+entry **hashmap_entry_set(hashmap *map);
 
-char **hashmap_key_set(hashmap_t *map);
+char **hashmap_key_set(hashmap *map);
 
-char **hashmap_values(hashmap_t *map);
+char **hashmap_values(hashmap *map);
 
-char *hashmap_put_val(hashmap_t *map, int hash, char *key, char *value, int onlyIfAbsent, int evict);
+char *hashmap_put_val(hashmap *map, int hash, char *key, char *value, int onlyIfAbsent, int evict);
 
-char *hashmap_put(hashmap_t *map, char *key, char *value);
+char *hashmap_put(hashmap *map, char *key, char *value);
 
-void hashmap_put_map_entries(hashmap_t *map, hashmap_t *m, int evict);
+void hashmap_put_map_entries(hashmap *map, hashmap *m, int evict);
 
-void hashmap_put_all(hashmap_t *map, hashmap_t *m);
+void hashmap_put_all(hashmap *map, hashmap *m);
 
-node_t *hashmap_get_node(hashmap_t *map, char *key);
+hashmap_node *hashmap_get_node(hashmap *map, char *key);
 
-char *hashmap_get(hashmap_t *map, char *key);
+char *hashmap_get(hashmap *map, char *key);
 
-node_t *hashmap_remove_node(hashmap_t *map, int hash, char *key, char *value, int matchValue, int movable);
+hashmap_node *hashmap_remove_node(hashmap *map, int hash, char *key, char *value, int matchValue, int movable);
 
-char *hashmap_remove(hashmap_t *map, char *key);
+char *hashmap_remove(hashmap *map, char *key);
 
-int hashmap_contains_key(hashmap_t *map, char *key);
+int hashmap_contains_key(hashmap *map, char *key);
 
-int hashmap_contains_value(hashmap_t *map, char *value);
+int hashmap_contains_value(hashmap *map, char *value);
 
-char *hashmap_get_or_default(hashmap_t *map, char *key, char *defaultValue);
+char *hashmap_get_or_default(hashmap *map, char *key, char *defaultValue);
 
-char *hashmap_put_if_absent(hashmap_t *map, char *key, char *value);
+char *hashmap_put_if_absent(hashmap *map, char *key, char *value);
 
-char *hashmap_replace(hashmap_t *map, char *key, char *value);
+char *hashmap_replace(hashmap *map, char *key, char *value);
