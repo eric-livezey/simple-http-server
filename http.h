27f8f8c6361e069da89f61119a0d016c3f6d60b7
eric-livezey <146676972+eric-livezey@users.noreply.github.com>
2024-04-16 19:52:24 -0400

    Update http.h

--- http.h
+++ http.h
@@ -1,44 +1,49 @@
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
 #include <unistd.h>
 #include "hashmap.h"
-#include "utils.h"
 
 typedef struct HTTP_request_s
 {
     char *method;
     char *path;
-    hashmap_t *query;
+    hashmap *query;
     char *protocol;
-    hashmap_t *headers;
+    hashmap *headers;
     char *body;
     unsigned long content_length;
-} HTTP_request_t;
+} HTTP_request;
 
 typedef struct HTTP_response_s
 {
     char *protocol;
     short code;
     char *reason;
-    hashmap_t *headers;
+    hashmap *headers;
     char *body;
     unsigned long content_length;
-} HTTP_response_t;
+} HTTP_response;
 
-int HTTP_request_destroy(HTTP_request_t *request);
+void urldecode(char *dst, const char *src);
 
-int HTTP_response_destroy(HTTP_response_t *response);
+int HTTP_request_free(HTTP_request *request);
 
-char *HTTP_reason(unsigned short code);
+int HTTP_response_free(HTTP_response *response);
 
-char *HTTP_date_r(struct tm *tm, char *result);
+char *HTTP_date_ex(struct tm *tm, char *result);
 
 char *HTTP_date(struct tm *tm);
 
-HTTP_request_t *HTTP_parserequest(char *data, HTTP_request_t *result);
+char *HTTP_reason(unsigned short code);
 
-long HTTP_ressize(HTTP_response_t *res);
+HTTP_request *HTTP_parserequest(char *data, HTTP_request *result);
 
-char *HTTP_resmsg(HTTP_response_t *res, char *result);
+long HTTP_reqsize(HTTP_request *req);
 
-int HTTP_response(int fd, HTTP_response_t *res);
+char *HTTP_reqmsg(HTTP_request *req, char *result);
+
+long HTTP_ressize(HTTP_response *res);
+
+char *HTTP_resmsg(HTTP_response *res, char *result);
+
+long HTTP_send_response(HTTP_response *res, int connfd);
